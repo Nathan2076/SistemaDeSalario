058c22ad352c6c07863af1cdd2c2b4b0863aecdf
Nathan2076 <68337317+Nathan2076@users.noreply.github.com>
2024-03-27 12:47:43 -0300

    Initial commit, with most things done

---
+++ README.md
@@ -0,0 +1,34 @@
+<h1 align="center">Sistema de Salário</h1>
+<p align="center">Uma calculadora de salários sob comissão com o uso de porcentagens</p>
+<p align="center">
+  <img alt="Exemplo" src="docs/Exemplo.gif" />
+</p>
+<br />
+
+## Sobre
+
+Este trabalho avaliativo de PHP da disciplina de Programação Web II envolve o cálculo de salários, utilizando porcentagens no contexto de ganhos sob comissão.
+
+## A atividade
+
+Todos os vendedores de uma loja possuem um salário mínimo de R$ 1927,02 e, com a nova implementação de um sistema de meritocracia, todos possuem uma meta de vendas semanais de R$ 20.000 (R$ 80.000 por mês), que é adicionada ao salário mínimo.
+
+- Todas as semanas que alcançaram R$ 20.000 em vendas recebem 1% do valor das vendas;
+- Todas as semanas que ultrapassaram R$ 20.000 recebem 1% de 20.000 + 5% do excedente;
+- Semanas que não alcançaram R$ 20.000 não recebem bônus.
+
+Caso a meta de todas as semanas tenha sido alcançada, um bônus de 10% sobre o excedente do mês é aplicado. Caso apenas 1 semana não tenha alcançado a meta de R$ 20.000, o bônus do mês não é aplicado.
+
+Ao final, o programa exibe na tela o salário que o vendedor receberá no mês, com base nos valores introduzidos.
+
+## Tecnologias utilizadas
+
+- ✅ PHP
+- ✅ HTML
+- ✅ CSS
+
+## Fontes consultadas
+
+- [Governador confirma novo Piso Regional do Paraná](https://www.aen.pr.gov.br/Noticia/Maior-do-Brasil-governador-confirma-novo-Piso-Regional-que-vai-de-R-18-mil-R-21-mil) (Agência Estadual de Notícias)
+- [foreach - Manual](https://www.php.net/manual/pt_BR/control-structures.foreach.php) (PHP)
+- [PSR-12: Extended Coding Style](https://www.php-fig.org/psr/psr-12/) (PHP-FIG)
---
+++ index.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>PWII - AVP I</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" media="screen" href="style.css">
+</head>
+<body>
+  <section>
+    <h1>Sistema de salário</h1>
+    <div>
+      <form method="post">
+        <label for="name">Nome</label><br />
+        <input type="text" name="name" /><br />
+        <br />
+        <input type="number" step="0.01" name="week1" placeholder="Valor semana 1" />
+        <input type="number" step="0.01" name="week2" placeholder="Valor semana 2" />
+        <br />
+        <input type="number" step="0.01" name="week3" placeholder="Valor semana 3" />
+        <input type="number" step="0.01" name="week4" placeholder="Valor semana 4" /><br />
+        <br />
+        <input type="submit" name="submit" value="Calcular" id="submit" />
+      </form>
+    </div>
+    <div id="message">
+      <?php
+          // Get user input
+
+          $name  = filter_input(INPUT_POST, "name");
+          $week1 = filter_input(INPUT_POST, "week1");
+          $week2 = filter_input(INPUT_POST, "week2");
+          $week3 = filter_input(INPUT_POST, "week3");
+          $week4 = filter_input(INPUT_POST, "week4");
+
+          // Guards
+
+          if (!($name && $week1 && $week2 && $week3 && $week4)) {
+              echo "Favor informar todos os dados.";
+              return;
+          }
+        
+          if ($week1 < 0 || $week2 < 0 || $week3 < 0 || $week4 < 0) {
+              echo "Favor informar valores positivos.";
+              return;
+          }
+
+          // Variables used
+        
+          $weeks = [ $week1, $week2, $week3, $week4 ];
+          $month = $week1 + $week2 + $week3 + $week4;
+          $monthBonus = true; 
+          $salary = 1927.02;
+        
+          // The salary is initialized with the minimum value of R$1927.02,
+          // and the additional values earned by commission are added directly.
+
+          foreach ($weeks as $week) {
+              if ($week < 20000) {
+                  $monthBonus = false;
+              } else {
+                  $salary += 200;
+                  $salary += ($week - 20000) * 0.05;
+              }
+          }
+
+          if ($monthBonus)
+              $salary += ($month - 80000) * 0.1;
+
+          echo "O vendedor $name receberá R$ $salary ao final do mês.";
+      ?>
+    </div>
+  </section>
+</body>
+</html>
